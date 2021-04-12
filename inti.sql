3258a6aaa144ecf482321bf424b37c19f8a2696f
jason <jasonalvarezcoraiza@gmail.com>
2021-04-12 11:42:58 -0400

    database structure init

---
+++ inti.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+    ID SERIAL PRIMARY KEY,
+    firstname TEXT,
+    lastname TEXT,
+    email TEXT,
+    birthday DATE,
+    encrypted_password TEXT
+);
+
+CREATE TABLE budgets (
+    ID SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    title TEXT,
+    description TEXT NOT NULL,
+    budget_amount MONEY,
+    banner_style INT REFERENCES banners(id),
+);
+
+CREATE TABLE items (
+    ID SERIAL PRIMARY KEY,
+    budget_id INT REFERENCES banners(id),
+    price MONEY,
+    rating INT,
+    constraint rating_range_check
+        check(rating >= 0 and rating <= 5),
+    title TEXT,
+    description TEXT NOT NULL,
+    count TINYINT,
+    purchase_date DATE
+);
+
+CREATE TABLE banners (
+    ID SERIAL PRIMARY KEY,
+    name TEXT
+);
