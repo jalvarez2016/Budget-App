3ae14edd609318b1678c96cf9b497e17df6a82e1
jason <jasonalvarezcoraiza@gmail.com>
2021-04-15 16:27:58 -0400

    fixed analyzeBudget.js

--- routes/itemsRouter.js
+++ routes/itemsRouter.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router();
 const itemsController = require('../controllers/itemsController');
+const userController = require("../controllers/usersController");
 
+router.use('/:id', userController.verifyUser)
 router.get('/:id', itemsController.getItem);
 router.get('/:id/edit', itemsController.editItem);
 
