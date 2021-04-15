483ccaf1918beb1017a6ad5b7daea9b69332cebe
jason <jasonalvarezcoraiza@gmail.com>
2021-04-15 13:16:35 -0400

    analyze page shows percentage of spending

---
+++ public/js/analyzeBudget.js
@@ -0,0 +1,37 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    main();
+});
+
+function main() {
+    percentSpent();
+}
+
+function percentSpent() {
+    const canvas = document.querySelector('#percent-spent');
+    if(canvas) {
+        const percent = Number(document.querySelector('#percent-spent-value').textContent);
+        const total = Number(document.querySelector("#percent-total-value-int").textContent);
+        const spent = Number(document.querySelector("#percent-spent-value-int").textContent);
+        const ctx = canvas.getContext("2d");
+        new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: [`${percent}% Spent`, `${100 - percent}% Left`],
+                datasets: [
+                    {
+                        label: 'Percent Spent',
+                        data: [spent, total],
+                        backgroundColor: ["#DC143C", "#00FF00"],
+                    }
+                ],
+            },
+            options: {
+                title: {
+                  display: true,
+                  text: 'Percent Spent'
+                }
+            },
+        });
+    }
+}
